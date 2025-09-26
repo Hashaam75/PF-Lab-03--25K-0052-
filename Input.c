1e3332608c94c2c2d8d728c2227ac12bf35c62ac
Mian Muhammad Hashaam <k250052@nu.edu.pk>
2025-09-26 06:55:21 -0700

    Create Input.c

---
+++ Input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char name[100];
+    int age;
+
+    printf("Enter your name: ");
+    scanf("%99s", name);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Hello %s, you are %d years old\n", name, age);
+
+    return 0;
+}
