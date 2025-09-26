49abbc6a6452854115d640a549ab5acdd8d7d29b
Mian Muhammad Hashaam <k250052@nu.edu.pk>
2025-09-26 06:59:31 -0700

    Create Escape.c

---
+++ Escape.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Muhammad Hashaam\n");
+    printf("Roll Number: 25K-0052\n");
+    printf("\\tTabbed Line\n");
+    printf("\\nNew Line\n");
+    return 0;
+}
